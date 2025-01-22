32c454d892eaf19307d00f0ad41d332f6b8b06a9
Pedro Crisanto <pedrohcrisanto@gmail.com>
2025-01-22 17:56:46 -0300

    configurations gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
   resources :contacts
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
+  mount Rswag::Ui::Engine => "/api-docs"
+  mount Rswag::Api::Engine => "/api-docs"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  get "contacts/search_address" => "contacts#search_address"
 
   # Defines the root path route ("/")
   # root "posts#index"
   devise_for :users,
              controllers: {
-               sessions: 'users/sessions',
-               registrations: 'users/registrations'
+               sessions: "users/sessions",
+               registrations: "users/registrations"
              }
 end
